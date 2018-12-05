28e6ee20a03704add0155d72e387f60c329b9fee
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-12-05 00:11:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  get '/newteam' do
+  		erb :index
+  	end
 
-
+  post '/team' do
+    erb :id
+  end
+# GET   Team name ('name') Coach ('coach') Point Guard ('pg') Shooting Guard ('sg') Power Forward ('pf') Small Forward ('sf') Center ('c')
 end
